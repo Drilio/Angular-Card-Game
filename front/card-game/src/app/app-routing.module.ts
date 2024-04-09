4fb59f9690359d013b89325cc2ce005f27fa43a7
Antoine <antoine.roy@tetras-libre.fr>
2024-04-09 12:06:43 +0200

    set up sign-up page

--- front/card-game/src/app/app-routing.module.ts
+++ front/card-game/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {LoginFormComponent} from "./login-module/login-form/login-form.component";
+import {SignUpFormComponent} from "./sign-up-module/sign-up-form/sign-up-form.component";
 
 const routes: Routes = [
   {path:'',component: HomeComponent},
-  {path:'login',component:LoginFormComponent}
+  {path:'login',component:LoginFormComponent},
+  {path:'sign-up', component:SignUpFormComponent},
 ];
 
 @NgModule({
