147b8766574bc0695f040b2df43ac19e870e5050
Antoine <antoine.roy@tetras-libre.fr>
2024-04-10 11:53:44 +0200

    Implementation of carousel comp

--- front/card-game/src/app/app.module.ts
+++ front/card-game/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import {NgOptimizedImage} from "@angular/common";
 import {LoginModule} from "./login-module/login/login.module";
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home-module/home/home.component';
 import { ReactiveFormsModule} from "@angular/forms";
 import {SignUpModule} from "./sign-up-module/sign-up/sign-up.module";
+import {HomePageModule} from "./home-module/home-page/home-page.module";
 
 @NgModule({
   declarations: [
@@ -24,6 +23,7 @@ import {SignUpModule} from "./sign-up-module/sign-up/sign-up.module";
     LoginModule,
     SignUpModule,
     ReactiveFormsModule,
+    HomePageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/card-game/src/app/home-module/home-page/home-page.module.ts
+++ front/card-game/src/app/home-module/home-page/home-page.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from "../home/home.component";
+import { CarouselComponent } from '../carousel/carousel.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    CarouselComponent,
   ],
   imports: [
     CommonModule
