1e1ad99a220790d692a9a54a96550212aea56f9d
Antoine <antoine.roy@tetras-libre.fr>
2024-04-10 10:05:21 +0200

    form Control Validation

--- front/card-game/src/app/login-module/login-form/login-form.component.ts
+++ front/card-game/src/app/login-module/login-form/login-form.component.ts
@@ -1,29 +1,82 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
+import {IUser} from "../../interfaces/IUser";
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css']
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit{
 
-  loginForm:FormGroup = this._formBuilder.group({
+  user:IUser = {
     login:"",
-    password:"",
-  })
+    password:""
+  }
+
+  loginForm: FormGroup = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  submitted = false;
+
+
+  homeUrl:string = '/'
+  loginError:boolean = false;
 
   constructor(
     private _formBuilder: FormBuilder,
     private _userService: UserService,
+    private _router:Router,
   ) {
   }
 
-  onSubmit( ){
-    console.warn('Your account was created successfully.', this.loginForm.value);
-    this._userService.login(this.loginForm.value);
+  ngOnInit(): void {
+    this.loginForm = this._formBuilder.group(
+      {
+        login: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20)
+          ]
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(40)
+          ]
+        ],
+      },
+    );
+  }
 
+  get f(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+
+  async onSubmit() {
+    this.user.login = this.loginForm.value.login;
+    this.user.password = this.loginForm.value.password;
+
+    this.submitted = true;
+
+    if(this.loginForm.invalid){
+      return;
+    }
+
+    const connection = await this._userService.login(this.user);
+    if (connection) {
+      await this._router.navigateByUrl(this.homeUrl);
+    }else{
+      this.loginError = true;
+    }
   }
 
 }
--- front/card-game/src/app/sign-up-module/sign-up-form/sign-up-form.component.ts
+++ front/card-game/src/app/sign-up-module/sign-up-form/sign-up-form.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {UserService} from "../../services/user.service";
+import {Router} from "@angular/router";
+import {IUser} from "../../interfaces/IUser";
 
 @Component({
   selector: 'app-sign-up-form',
@@ -8,21 +10,71 @@ import {UserService} from "../../services/user.service";
   styleUrls: ['./sign-up-form.component.css'],
 })
 
-export class SignUpFormComponent {
+export class SignUpFormComponent implements OnInit {
+
+  user:IUser = {
+    login:"",
+    password:""
+  }
 
   signUpForm:FormGroup = this._formBuilder.group({
-    login:"",
-    password:"",
+    login: new FormControl(''),
+    password: new FormControl(''),
   })
+
+  submitted = false;
+
+  homeUrl:string = '/'
 
   constructor(
     private _formBuilder: FormBuilder,
     private _userService: UserService,
+    private _router:Router,
   ) {
   }
 
+  ngOnInit(): void {
+    this.signUpForm = this._formBuilder.group(
+      {
+        login: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20)
+          ]
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(40)
+          ]
+        ],
+      },
+    );
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.signUpForm.controls;
+  }
+
+
   onSubmit( ){
-    console.warn('Your account was created successfully.', this.signUpForm.value);
-    return this._userService.CreateUser(this.signUpForm.value);
+
+    this.user.login = this.signUpForm.value.login;
+    this.user.password = this.signUpForm.value.password;
+
+    this.submitted = true;
+
+    if(this.signUpForm.invalid){
+      return;
+    }
+
+    console.warn('Your account was created successfully.', this.user);
+    const connect = this._userService.CreateUser(this.user);
+    return this._router.navigateByUrl(this.homeUrl);
+
   }
 }
